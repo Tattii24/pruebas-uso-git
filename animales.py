277bf7d17854f8f87a846ef6b591df7dd49d3bce
Tatiana Taborda <tatianataborda46@gmail.com>
2025-04-08 16:09:07 -0500

    Se agrega metodo ladrar

---
+++ animales.py
@@ -0,0 +1,10 @@
+class Perro():
+
+    def __init__(self, nombre, raza, edad):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+
+    def ladrar (self):
+            return f'{"guau*10"}'
+    
